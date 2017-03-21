e215891a890a03147b9e775d440236204e237d83
pikinder <p.kindermans@tu-berlin.de>
2017-03-21 15:13:05 +0100

    Added the checkpoint after 1000 episodes Updated the file for the evaluation of the agent to use this..

--- evaluate_agent.py
+++ evaluate_agent.py
@@ -8,21 +8,18 @@ import numpy as np
 from agent import QAgent
 from configs import pong_config
 
+
 if __name__ == '__main__':
     config = pong_config
-    config['state_memory']=1 # prevent allocating a lot
+    config['state_memory']=1 # prevent allocating of a huge chunk of memory
+    epsilon = 0.00 # The epsilon for the strategy
+    params = 'saves/PONG_1000.ckpt'
+    agent = QAgent(config=config, log_dir=None)
+    tf.train.Saver().restore(agent.session,params)
 
-    param_dir = 'log/2017-03-20_17-01-26_Pong-v0_True'
-    load_episode = 1000
-
-    log_dir = None#get_log_dir('log', 'evaluation_'+config['game']+'_'+str(config['double_q']))
-    with tf.device('/cpu:0'):
-        agent = QAgent(config=config, log_dir=log_dir)
-    saver = tf.train.Saver()
-    saver.restore(agent.session,'%s/episode_%d.ckpt'%(param_dir,load_episode))
-    agent.assign_train_to_target()
-
+    # Play the game
     while True:
+        # Initielise the episode
         state = agent._reset_state()
         done = False
         total_reward = 0.
@@ -30,18 +27,23 @@ if __name__ == '__main__':
         # Prepare the visualisation
         plt.figure('q_network')
         plt.clf()
-        plt.subplot(3,1,1)
+        plt.subplot(2,1,1)
         ax = plt.gca()
         bar = ax.bar(range(config['actions']),np.zeros(config['actions']))
         plt.xticks(.5+np.r_[:config['actions']],['None','None','Up','Down','Up','Down'])
         plt.ylabel('q[a]/max|q|')
-        plt.subplot(3,1,2)
+        plt.subplot(2,1,2)
         ax = plt.gca()
-        line_q, = ax.plot([], [], 'k', label='Q(s)', lw=2)
-        #plt.subplot(3,1,3)
-        #ax = plt.gca()
-        line_r, = ax.plot([], [], 'xr', label='R(s)', lw=2,markersize=20)
-
+        line_q, = ax.plot([], [], 'k', label='max(Q(s,.))', lw=2)
+        line_r, = ax.plot([], [], 'or', label='R(s,a)', lw=10)
+        # Shrink current axis by 20%
+        plt.legend()
+        box = ax.get_position()
+        ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+        # Put a legend to the right of the current axis
+        ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+        plt.xlabel('frames')
+        plt.ylabel('reward')
         qs = []
         rx = []
         rs = []
@@ -63,13 +65,10 @@ if __name__ == '__main__':
                 rect.set_height(yi/np.abs(q).max())
             line_q.set_data(range(len(qs)),qs)
             line_r.set_data(rx,rs)
-            plt.subplot(3,1,1)
+            plt.subplot(2,1,1)
             plt.xlim(0,q.shape[1]-1)
             plt.ylim(-1,1)#[i*np.abs(q).max() for i in [-1.,1.]])
-            plt.subplot(3,1,2)
-            plt.xlim(0,len(qs))
-            plt.ylim(-2,2)
-            plt.subplot(3,1,3)
+            plt.subplot(2,1,2)
             plt.xlim(0,len(qs))
             plt.ylim(-2,2)
 
